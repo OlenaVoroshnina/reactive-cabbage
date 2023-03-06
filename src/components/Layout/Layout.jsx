6df5324a887cdd4d8e9a8c78ddd0c5af8b58831a
Olena Voroshnina <yuriyilena67@gmail.com>
2023-03-06 18:45:08 +0100

    update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,19 +2,16 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Container from "components/Container/Container";
 import { Header } from "components/Header/Header";
+import { Loader } from "components/Loader/Loader";
 
 export const Layout = () => {
 
     return (
     <Container>
       <Header />
-     
-      <Suspense 
-    //   fallback={<Loader />}
-      >
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
-
     </Container>
     )
 };
