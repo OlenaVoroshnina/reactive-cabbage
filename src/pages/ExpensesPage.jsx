90e4ce1ceaeed97b044eef7ddfb774783db685a6
YDaKota <95923839+YDaKota@users.noreply.github.com>
2023-03-07 18:46:20 +0200

    add inputs

--- src/pages/ExpensesPage.jsx
+++ src/pages/ExpensesPage.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectExpenses, selectIsLoggedIn, selectBalance } from "redux/selectors";
+import { getExpense } from "redux/transactions/operation";
+import { InputForm } from "components/InputForm/InputForm";
+
 export const ExpensesPage = () => {
+    const dispatch = useDispatch();
+    const expenses = useSelector(selectExpenses);
+    const user = useSelector(selectIsLoggedIn);
+    const balance = useSelector(selectBalance);
+
+    useEffect(() => {
+        if (user) {
+            dispatch(getExpense());
+            }
+        }, [dispatch, user, balance]);
 
     return (
-        <></>
+        <>
+            <div>
+                <InputForm />
+            </div>
+        </>
     )
 };
