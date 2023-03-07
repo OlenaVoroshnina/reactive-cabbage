fd19f618d237ee52bbe33ba31df9a617f079b353
Tishko1 <113176690+Tishko1@users.noreply.github.com>
2023-03-07 23:32:13 +0300

    change slice

--- src/redux/auth/authSlice.jsx
+++ src/redux/auth/authSlice.jsx
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import * as authApi from 'redux/auth/operation';
+import {register, logIn, logOut, getAllUserInfo, refreshToken} from 'redux/auth/operation';
 
 const initialState = {
   accessToken: '',
@@ -35,53 +35,53 @@ const authSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(authApi.register.pending, handlePending)
-      .addCase(authApi.register.fulfilled, (state, action) => {
+      .addCase(register.pending, handlePending)
+      .addCase(register.fulfilled, (state, action) => {
         state.userId = action.payload.id;
       })
-      .addCase(authApi.register.rejected, handleRejected)
+      .addCase(register.rejected, handleRejected)
 
-      .addCase(authApi.logIn.pending, handlePending)
-      .addCase(authApi.logIn.fulfilled, (state, action) => {
+      .addCase(logIn.pending, handlePending)
+      .addCase(logIn.fulfilled, (state, action) => {
         state.accessToken = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
         state.sid = action.payload.sid;
         state.userEmail = action.payload.userData.email;
         state.isLoggedIn = true;
       })
-      .addCase(authApi.logIn.rejected, handleRejected)
+      .addCase(logIn.rejected, handleRejected)
 
-      .addCase(authApi.logOut.pending, handlePending)
-      .addCase(authApi.logOut.fulfilled, (state, action) => {
+      .addCase(logOut.pending, handlePending)
+      .addCase(logOut.fulfilled, (state, action) => {
         state.userId = null;
         state.accessToken = null;
         state.refreshToken = null;
         state.sid = null;
         state.isLoggedIn = false;
       })
-      .addCase(authApi.logOut.rejected, handleRejected)
+      .addCase(logOut.rejected, handleRejected)
 
-      .addCase(authApi.refreshToken.pending, (state, action) => {
+      .addCase(refreshToken.pending, (state, action) => {
         state.isFetchingCurrentUser = true;
       })
-      .addCase(authApi.refreshToken.fulfilled, (state, action) => {
+      .addCase(refreshToken.fulfilled, (state, action) => {
         state.accessToken = action.payload.newAccessToken;
         state.refreshToken = action.payload.newRefreshToken;
         state.sid = action.payload.newSid;
         state.isLoggedIn = true;
         state.isFetchingCurrentUser = false;
       })
-      .addCase(authApi.refreshToken.rejected, (state, action) => {
+      .addCase(refreshToken.rejected, (state, action) => {
         state.isFetchingCurrentUser = false;
       })
 
-      .addCase(authApi.getAllUserInfo.pending, handlePending)
-      .addCase(authApi.getAllUserInfo.fulfilled, (state, action) => {
+      .addCase(getAllUserInfo.pending, handlePending)
+      .addCase(getAllUserInfo.fulfilled, (state, action) => {
         state.transactions = action.payload.transactions;
         state.userEmail = action.payload.email;
         state.isLoggedIn = true;
       })
-      .addCase(authApi.getAllUserInfo.rejected, handleRejected);
+      .addCase(getAllUserInfo.rejected, handleRejected);
   },
 });
 
