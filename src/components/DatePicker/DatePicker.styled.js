b392e24a0ab6b76c6d18cfda8a18de19349f6119
OlesiaMelnyk1 <elsonidodelmar26@gmail.com>
2023-03-07 21:02:20 +0200

    add dataPicker

---
+++ src/components/DatePicker/DatePicker.styled.js
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+
+export const StyledDatePicker = styled.div`
+  .calendarIcon {
+    width: 20px;
+    height: 20px;
+    fill: #52555f;
+  }
+  .datePicker {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    height: 20px;
+    font-size: 12px;
+    font-weight: 900;
+    line-height: calc(14 / 12);
+    letter-spacing: 1.04;
+    color: #52555f;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    :hover,
+    :focus {
+      color: #ff751d;
+    }
+    :hover .calendarIcon,
+    :focus .calendarIcon {
+      fill: #ff751d;
+    }
+  }
+`;
