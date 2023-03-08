77efbdcd7643a8b07ceb1b5ebdc944ad9e81b10a
Daria Hryshchenko <daria199210@gmail.com>
2023-03-08 12:12:23 +0200

    fix duplicate login form

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import { useAuth } from './../hooks/useAuth';
-// import { UserNav } from 'components/UserNav/UserNav';
-// import { Header } from 'components/Header/Header';
 import { LoginPage } from 'pages/LoginPage';
 import { HomePage } from './HomePage';
 
