5e9bab780270a8faf65caee9b2ed7c2d79481d3b
Olena Voroshnina <yuriyilena67@gmail.com>
2023-03-09 12:34:39 +0100

    display-none

--- src/components/Transactions/ListTransactions.styled.js
+++ src/components/Transactions/ListTransactions.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledList = styled.ul`
   width: 320px;
   @media screen and (min-width: 768px) {
-    display: none;
+    /* display: none; */
   }
 `;
 
