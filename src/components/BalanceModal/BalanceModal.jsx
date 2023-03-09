2a086b1ed8a9d81389a2c7bb2ea4aca6b9553645
Olena Voroshnina <yuriyilena67@gmail.com>
2023-03-09 14:36:19 +0100

    fixed warning&error

--- src/components/BalanceModal/BalanceModal.jsx
+++ src/components/BalanceModal/BalanceModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   StyledBackdrop,
