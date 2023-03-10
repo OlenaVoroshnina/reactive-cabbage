cf5ec143fe6167d13c63a5d0adfce01f959d4f95
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-03-10 18:30:58 +0200

    add TransactionsTable

---
+++ src/components/TransactionsTable/TransactionsTable.jsx
@@ -0,0 +1,100 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { translateToEng } from 'hooks/useCategory';
+import { TransactionTable } from './TransactionsTable.styled';
+import { selectExpenses, selectIncomes } from 'redux/selectors';
+import { useLocation } from 'react-router-dom';
+
+import { deleteTransaction } from '../../redux/transactions/operation';
+import { ReactComponent as DeleteIcon } from '../../images/delete.svg';
+import { useState } from 'react';
+import UniversalModal from 'components/UniversalModal/UniversalModal';
+
+export const TransactionsTable = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+
+  const expenses = useSelector(selectExpenses);
+  const incomes = useSelector(selectIncomes);
+  const location = useLocation();
+  let transactions = location.pathname === '/expenses' ? expenses : incomes;
+
+  const dispatch = useDispatch();
+  const color = transactions[1];
+  let minus = '-';
+  if (color === 'green') {
+    minus = false;
+  }
+
+  const handleModalOpen = id => {
+    setModalOpen(true);
+    setCurrentId(id);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleDelete = event => {
+    dispatch(deleteTransaction(currentId));
+    setCurrentId(null);
+    setModalOpen(false);
+  };
+
+  const sortedTransactions = transactions.slice().sort((a, b) => {
+    const first = new Date(a.date).getTime();
+    const second = new Date(b.date).getTime();
+    if (first - second === 0) {
+      return first;
+    }
+    return second - first;
+  });
+
+  return (
+    <>
+      <TransactionTable>
+        <thead>
+          <tr>
+            <th>DATE</th>
+            <th>DESCRIPTION</th>
+            <th>CATEGORY</th>
+            <th>SUM</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedTransactions.slice(0).map(el => {
+            const { _id, description, amount, date, category } = el;
+            return (
+              <tr key={_id} style={{ height: 40 }}>
+                <td>{date.split('-').reverse().join('.')}</td>
+                <td>{description}</td>
+                <td>{translateToEng(category)}</td>
+                <td style={{ color }}>
+                  {minus} {amount.toFixed(2)}
+                </td>
+                <td>
+                  <span
+                    id={_id}
+                    onClick={() => handleModalOpen(_id)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <DeleteIcon />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </TransactionTable>
+      {modalOpen && (
+        <UniversalModal
+          closeModal={handleModalClose}
+          onDelete={handleDelete}
+          text="SURE"
+        >
+          Are you sure?
+        </UniversalModal>
+      )}
+    </>
+  );
+};
