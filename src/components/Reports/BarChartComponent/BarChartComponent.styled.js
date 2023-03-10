8328832ff268eb5ac6e75cf1677af2ee546468f3
Olha <cukbu30@gmai.com>
2023-03-10 22:43:28 +0200

    Update BarChartComponent.styled.js

--- src/components/Reports/BarChartComponent/BarChartComponent.styled.js
+++ src/components/Reports/BarChartComponent/BarChartComponent.styled.js
@@ -12,19 +12,3 @@ export const BarMain = styled.div`
   background-color: #fff;
   box-sizing: revert;
 `;
-
-// import styled from '@emotion/styled';
-
-// export const BarMain = styled.div`
-//   font-size: 10px;
-//   width: 93vw;
-//   height: 100vh;
-//   @media screen and (min-width: 768px) {
-//     width: 95vw;
-//     height: 50vh;
-//   }
-//   @media screen and (min-width: 1280px) {
-//     width: 82vw;
-//     height: 50vh;
-//   }
-// `;
