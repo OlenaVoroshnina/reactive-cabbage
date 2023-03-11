04a6b7b3f82fb4a7795def5e7a681594501f14ef
Olena Voroshnina <yuriyilena67@gmail.com>
2023-03-11 20:59:10 +0100

    delete svg

--- src/images/imgReports/handy-svg/lib/fetcher.d.ts
+++
@@ -1,6 +0,0 @@
-interface RequestParams extends RequestInit {
-    timeout?: number;
-    retryCount?: number;
-}
-export declare function fetchSvg(url: string, params?: RequestParams): Promise<string>;
-export {};
